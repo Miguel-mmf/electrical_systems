dbb07372ff5c83203f89e4e4b2d882363d0c0908
Miguel <miguel.ferreira@cear.ufpb.br>
2023-03-22 01:37:23 +0000

    new functions

--- utils/convert_phasor.m
+++
@@ -1,5 +0,0 @@
-function c_num = convert_phasor(mod,ang_d)
-    %UNTITLED Summary of this function goes here
-    %   Detailed explanation goes here
-    c_num = mod*(cosd(ang_d) + 1i*sind(ang_d));
-end
--- utils/get_phasor.m
+++
@@ -1,5 +0,0 @@
-function get_phasor(phasor)
-    
-    fprintf(" %.4f/%.4f\n",abs(phasor),angle(phasor)*180/pi);
-end
-
